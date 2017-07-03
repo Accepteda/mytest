17fffa6d0502e7bfd8106bfc51b2d5c5c18f832c
Accepteda <924913496@qq.com>
2017-07-03 11:09:26 +0800

    first commit

---
+++ Test/com/ywq1/Test_juintTest.java
@@ -0,0 +1,31 @@
+package com.ywq1;
+
+import junit.framework.TestCase;
+
+public class Test_juintTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testTestAddNum() {
+		fail("Not yet implemented");
+	}
+
+	public void testTestm2() {
+		fail("Not yet implemented");
+	}
+
+	public void testTestm3() {
+		fail("Not yet implemented");
+	}
+
+	public void testTestm4() {
+		fail("Not yet implemented");
+	}
+
+}
